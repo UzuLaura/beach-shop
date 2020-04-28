7f8cd458d30000dddf6a1ade6aecf1991b11e556
UzuLaura <58941369+UzuLaura@users.noreply.github.com>
2020-04-28 16:37:24 +0300

    added tags

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -4,21 +4,26 @@
         <div class="hero-body">
             <div class="container">
             <h1 class="title">Our Products</h1>
-            <h2 class="subtitle">Subtitle</h2>
+            <h2 class="subtitle">All Watches</h2>
             </div>
         </div>
     </section>
     <section>
         <div class="columns is-multiline">
-            <div class="column card is-3" v-for='product in products'
+            <div class="column is-half card is-3" v-for='product in products'
             :key='product.title'
             >
-                <div class="card-image">
-                    <img class="card-img" :src="product.img" :alt="product.title">
+                <div class="flex-card">
+                    <img class="product-img" :src="product.img" :alt="product.title">
                 </div>
                 <div class="card-content">
-                    <h2>{{ product.title }}</h2>
+                    <h2 class="title is-4">{{ product.title }}</h2>
+                    <span class="tag" v-bind:class="product.tagColor">{{ product.tag }}</span>
+                    <hr>
                     <p>{{ product.about }}</p>
+                    <hr>
+                    <h3 class="title is-4">Price: {{ product.price }}€</h3>
+                    <b-button>Add to Chart</b-button>
                 </div>
             </div>
         </div>
@@ -44,15 +49,34 @@ export default {
         .collection('watches')
         .get()
         .then(data => {
-          data.forEach(d => {
+          data.forEach(product => {
             const newObj = {
-              title: d.data().title,
-              img: d.data().img,
-              about: d.data().about.substring(1, 200)
+              title: product.data().title,
+              price: product.data().price,
+              img: product.data().img,
+              about: this.cutText(product.data().about),
+              tag: product.data().tag,
+              tagColor: this.setTagColor(product.data().tag)
             }
             this.products.push(newObj)
+            console.log(newObj)
           })
         })
+    },
+    cutText (string) {
+      if (string.length > 200) {
+        string = string.substring(0, 200) + '...'
+        return string
+      } else {
+        return string
+      }
+    },
+    setTagColor (tag) {
+      if (tag === 'female') {
+        return 'is-danger'
+      } else {
+        return 'is-success'
+      }
     }
   },
   beforeMount () {
@@ -62,8 +86,11 @@ export default {
 </script>
 
 <style>
+.flex-card {
+    display: flex;
+    justify-content: center;
+}
 .product-img {
     height: 300px;
-    text-align: center;
 }
 </style>
