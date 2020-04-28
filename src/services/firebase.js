5507774c219966f51929a54682ff5a1cb6ef7851
Pijus <user@users-MBP.lan>
2020-04-28 12:28:02 +0300

    init

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyCgUux3ooVntlKQaXh6Upmb-Q3hK5mROyA',
+  authDomain: 'beach-shop.firebaseapp.com',
+  databaseURL: 'https://beach-shop.firebaseio.com',
+  projectId: 'beach-shop',
+  storageBucket: 'beach-shop.appspot.com',
+  messagingSenderId: '1064645061840',
+  appId: '1:1064645061840:web:ed4a9aebdc84b6e33882d5'
+
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
